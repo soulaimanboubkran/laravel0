1900ad8c757d0f8387ad187499d2eb8bd8a56ac5
soul <soulaiman.soulb@gmail.com>
2023-11-03 23:05:05 +0100

    api post action

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -1,18 +1,46 @@
 <?php
-
 namespace App\Http\Controllers;
 
 use App\Models\Api1;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator; // Correctly import the Validator class
 
 class ApiController extends Controller
 {
     public function index(){
         $Api1 = Api1::all();
         $data = [
-            'persons'=>$Api1,
+            'persons' => $Api1,
         ];
-        return response()->json($data,200);
+        return response()->json($data, 200);
+    } 
 
+    public function upload(Request $request){ // Note that it should be $request, not $requist
+        $validator = Validator::make($request->all(), [
+            "name" => "required",
+            "email" => "required|email"
+        ]);
+
+        if ($validator->fails()) {
+            $data = [
+                "status"=>422,
+                "message"=>$validator->messages()
+            ];
+            return response()->json($data,422);
+        }else{
+            $person =new Api1;
+            $person->name=$request->name;
+            $person->email=$request->email;
+            $person->phone=$request->phone;
+
+            $person->save();
+
+            $data=[
+                "status"=>200,
+                "message"=>"Data has been uploaded successfully"
+            ];
+            return response()->json($data,200);
+        }
+        
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('person',[ApiController::class,'index']);
+Route::post('person',[ApiController::class,'upload']);

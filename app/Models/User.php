0745199e0178c34282c75954db58d19e87bf8bdb
soul <soulaiman.soulb@gmail.com>
2023-11-07 23:44:30 +0100

    Create Tasks For The Logged In User

--- app/Models/User.php
+++ app/Models/User.php
@@ -43,7 +43,7 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
-    
+     
     public function tasks():HasMany{
         return $this->hasMany(Task::class,'creator_id');
     }

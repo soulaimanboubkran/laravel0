d26f266c20f9fabbf67b16d3541ae739f7de0118
soul <soulaiman.soulb@gmail.com>
2023-11-10 01:10:49 +0100

    Deleting Projects  Foreign Key Constraints

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreProjectRequest;
 use App\Http\Requests\UpdateProjectRequest;
+use App\Http\Resources\ProjectCollect;
 use App\Http\Resources\ProjectRecource;
 use App\Models\Project;
 use Illuminate\Http\Request;
@@ -12,8 +13,11 @@ use Illuminate\Support\Facades\Auth;
 class ProjectController extends Controller
 {
     //
+    public function index(Request $request){
+        return new ProjectCollect(Auth::user()->projects()->paginate());
+    }
     public function show(Request $request, Project $project){
-        return new  ProjectRecource($project);
+        return (new  ProjectRecource($project))->load('tasks');
     }
     public function store(StoreProjectRequest $request){
         $validated = $request->validated();
@@ -25,5 +29,9 @@ class ProjectController extends Controller
         $validated = $request->validated();
         $project->update($validated);
         return new ProjectRecource($project);
+    }
+    public function destroy(Request $request,Project $project){
+        $project->delete();
+        return response()->noContent();
     }
 }
